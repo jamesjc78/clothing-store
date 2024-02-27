223fa44ef93be95aae09318520267ea7932f7c9d
jamesjc78 <jla@noaya.no>
2024-02-27 17:18:49 +0800

    setting homepage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
-import { useState } from "react";
-import { isEmpty } from "lodash";
-import Categories from "./components/categories/categories.component";
+import Home from "./routes/home/home.component";
+import { Routes,Route } from "react-router-dom";
 
-const App = () => {
-
-  const [categories,setCategories] = useState<Category[]>([
-      {
-        id: 1,
-        title: "hats",
-        imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        id: 2,
-        title: "jackets",
-        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        id: 3,
-        title: "sneakers",
-        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        id: 4,
-        title: "womens",
-        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        id: 5,
-        title: "mens",
-        imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
-      }
-  ])
-
-    return(
-      <div className="App">
-        {
-          isEmpty(categories)?
-            <div>No categories found!</div>
-          : 
-            <Categories categories={categories}/>
-        }
-      </div>
+const App = () =>{
+  
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  
 )}
 
 export default App;
---
+++ src/routes/home/home.component.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { isEmpty } from "lodash";
+import Categories from "../../components/categories/categories.component";
+
+const Home = () => {
+
+  const [categories,setCategories] = useState<Category[]>([
+      {
+        id: 1,
+        title: "hats",
+        imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+      },
+      {
+        id: 2,
+        title: "jackets",
+        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        id: 3,
+        title: "sneakers",
+        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+      },
+      {
+        id: 4,
+        title: "womens",
+        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+      },
+      {
+        id: 5,
+        title: "mens",
+        imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+      }
+  ])
+
+    return(
+      <div className="App">
+        {
+          isEmpty(categories)?
+            <div>No categories found!</div>
+          : 
+            <Categories categories={categories}/>
+        }
+      </div>
+)}
+
+export default Home;
